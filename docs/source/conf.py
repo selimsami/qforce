c7e95483a0e51f6ad10dd74d2fb0e252ae38d028
Selim Sami <s.sami@rug.nl>
2020-09-01 16:39:49 +0200

    preparing release

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,6 +27,9 @@ release = '1.0'
 
 # -- General configuration ---------------------------------------------------
 
+# master doc
+master_doc = 'index'
+
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
