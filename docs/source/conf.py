af16c4955b032e4451b5456904f4501d3a883b41
Selim Sami <s.sami@rug.nl>
2020-10-05 17:57:52 +0200

    docu fix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,8 +12,7 @@
 #
 import os
 import sys
-import colt
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('../../..'))
 
 # -- Project information -----------------------------------------------------
 
