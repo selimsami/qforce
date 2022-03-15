8773e11af115e200a9bf62bd21eb401a71f5491c
Maximilian Menger <menger.maximilian@gmail.com>
2022-03-15 20:43:29 +0100

    updated xtb interface

--- qforce/qm/xtb.py
+++ qforce/qm/xtb.py
@@ -1,3 +1,5 @@
+import numpy as np
+#
 from .gaussian import Gaussian, ReadGaussian, WriteGaussian
 
 
@@ -24,12 +26,43 @@ class ReadXTBGaussian(ReadGaussian):
 
     @classmethod
     def _read_esp_charges(cls, file, n_atoms):
-        return self._read_charges(file, n_atoms)
+        return cls._read_charges(file, n_atoms)
 
     @classmethod
     def _read_cm5_charges(cls, file, n_atoms):
-        return self._read_charges(file, n_atoms)
+        return cls._read_charges(file, n_atoms)
 
+    @staticmethod
+    def _read_nbo_analysis(file, line, n_atoms):
+        found_wiberg = False
+        lone_e = np.zeros(n_atoms, dtype=int)
+        n_bonds = []
+        b_orders = [[] for _ in range(n_atoms)]
+        while "--- END WBO ANALYSIS ---" not in line:
+            line = file.readline()
+            print(line)
+            if ("bond index matrix" in line and not found_wiberg):
+                for _ in range(int(np.ceil(n_atoms/9))):
+                    for atom in range(-3, n_atoms):
+                        line = file.readline().split()
+                        if atom >= 0:
+                            order = [float(line_cut) for line_cut in line[2:]]
+                            b_orders[atom].extend(order)
+            if ("bond index, Totals" in line and not found_wiberg):
+                found_wiberg = True
+                for i in range(-3, n_atoms):
+                    line = file.readline()
+                    if i >= 0:
+                        n_bonds.append(int(round(float(line.split()[2]), 0)))
+            if "Natural Bond Orbitals (Summary)" in line:
+                while "Total Lewis" not in line:
+                    line = file.readline()
+                    if " LP " in line:
+                        atom = int(line[19:23])
+                        occ = int(round(float(line[40:48]), 0))
+                        if occ > 0:
+                            lone_e[atom-1] += occ
+        return n_bonds, b_orders, lone_e
 
 class XTB(Gaussian):
 
