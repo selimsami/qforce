38b7c0a55b30f4e4ef527d489139da28164b94b5
Selim Sami <sesami141@gmail.com>
2022-05-06 16:43:19 +0200

    added charmmm36 to list of ffs

--- qforce/molecule/non_bonded.py
+++ qforce/molecule/non_bonded.py
@@ -201,6 +201,10 @@ def set_non_bonded_props(config):
         comb_rule = 2
         fudge_lj, fudge_q = 0.5, 0.8333
         h_cap = 'hc'
+    elif config.lennard_jones == 'charmm36':
+        comb_rule = 2
+        fudge_lj, fudge_q = 1.0, 1.0
+        h_cap = 'HGA2'
     return comb_rule, fudge_lj, fudge_q, h_cap
 
 
