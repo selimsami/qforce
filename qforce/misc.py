5671c9edfd4b93b4d334c53f4f600812fdadf504
Mateus Zanotto <39527468+mateuszanotto@users.noreply.github.com>
2022-09-15 10:28:36 -0300

    Added logo with hash (#) for .mol2

--- qforce/misc.py
+++ qforce/misc.py
@@ -27,6 +27,18 @@ LOGO_SEMICOL = """
 ;            ==============================
 """
 
+LOGO_HASH = """
+#          ____         ______
+#         / __ \       |  ____|
+#        | |  | |______| |__ ___  _ __ ___ ___
+#        | |  | |______|  __/ _ \| '__/ __/ _ \\
+#        | |__| |      | | | (_) | | | (_|  __/
+#         \___\_\      |_|  \___/|_|  \___\___|
+#
+#                     Selim Sami
+#            University of Groningen - 2020
+#            ==============================
+"""
 
 def check_if_file_exists(filename):
     if not os.path.exists(filename) and not os.path.exists(f'{filename}_qforce'):
