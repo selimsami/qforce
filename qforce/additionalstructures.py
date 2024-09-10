1d07a9b1e25107eefeef5366f61e7e417c1fd4b2
Maximilian Menger <menger.maximilian@gmail.com>
2024-09-10 18:57:15 +0200

    added weighted iterator to additionalstrucutres

--- qforce/additionalstructures.py
+++ qforce/additionalstructures.py
@@ -1,4 +1,5 @@
 import os
+from shutil import copy2 as copy
 #
 from colt import Colt
 from ase.io import read
@@ -9,9 +10,13 @@ class AdditionalStructureCreator(Colt):
 
     __classes = {}
 
+    def __init__(self, weight):
+        self.weight = weight
+
     def __init_subclass__(cls, **kwargs):
         super().__init_subclass__(**kwargs)
         cls.__classes[cls.name] = cls
+        cls._user_input += "\n# Weight of the structures in the forcefield fit\n weight = 1 :: int :: >1 \n"
 
     @classmethod
     def classes(cls):
@@ -35,7 +40,8 @@ class StructuresFromFile(AdditionalStructureCreator):
         hess_struct = :: existing_file, optional
     """
 
-    def __init__(self, en_struct, grad_struct, hess_struct):
+    def __init__(self, weight, en_struct, grad_struct, hess_struct):
+        self.weight = weight
         self._data = {
                 'en':  {'file': en_struct,
                         'calculations': [],
@@ -59,7 +65,7 @@ class StructuresFromFile(AdditionalStructureCreator):
     def from_config(cls, config):
         if config['en_struct'] is None and config['grad_struct'] is None and config['hess_struct'] is None:
             return None
-        return cls(config['en_struct'], config['grad_struct'], config['hess_struct'])
+        return cls(config['weight'], config['en_struct'], config['grad_struct'], config['hess_struct'])
 
     @staticmethod
     def _fileiter(filename):
@@ -120,12 +126,144 @@ class StructuresFromFile(AdditionalStructureCreator):
         grad['results'] = grad_results
         hess['results'] = results
 
+    def enouts(self):
+        return self._data['en']['results']
+
+    def gradouts(self):
+        return self._data['grad']['results']
+
+    def hessouts(self):
+        return self._data['hess']['results']
+
+
+class MetaDynamics:
+
+    name = 'metadynamics'
+
+    _user_input = """
+    # temperature in Kelvin
+    temp = 1000 :: float :: >0
+    # total simulation time in ps
+    time = 5.0 :: float :: >0
+    # interval for trajectory printout in ps
+    dump = 5.0 :: float :: >0
+    # time step for propagation in fs
+    step = 2.0 :: float :: >0
+    """
+
+    def __init__(self, weight, config):
+        self.weight = weight
+        self.xtbinput = {key: value for key, value in config.items() if key != 'weight'}
+
+    @classmethod
+    def from_config(cls, config):
+        if config['en_struct'] is None and config['grad_struct'] is None and config['hess_struct'] is None:
+            return None
+        return cls(config['weight'], config['en_struct'], config['grad_struct'], config['hess_struct'])
+
+    @staticmethod
+    def _fileiter(filename):
+        molecules = read(filename, index=':')
+        for i, molecule in enumerate(molecules):
+            yield i, (molecule.get_positions(), molecule.get_atomic_numbers())
+
+    def create(self, parent, qm):
+        folder = parent / '0_metadynamics'
+        os.makedirs(folder, exist_ok=True)
+
+        initfile = qm.pathways.getfile('init.xyz')
+        copy(initfile, folder / 'xtb.xyz')
+
+        with open(folder / 'md.inp', 'w') as fh:
+            fh.write("$md\n")
+            for key, value in self.xtbinput.items():
+                fh.write(f"{key} = {value}\n")
+            fh.write("$end\n")
+
+    def check(self):
+        return None
+
+    def parse(self, qm):
+        pass
+
+
+class HessianOutput:
+
+    def __init__(self, weight, hessout):
+        self.weight = weight
+        self.hessout = hessout
+
+    def enouts(self):
+        return []
+
+    def gradouts(self):
+        return []
+
+    def hessouts(self):
+        return [self.hessout]
+
+    def create(self, parent, qm):
+        ...
+
+    def parse(self, qm):
+        ...
+
+    def check(self):
+        ...
+
+
+class DihedralOutput:
+
+    def __init__(self, weight, gradouts):
+        self.weight = weight
+        self._gradouts = gradouts
+
+    def enouts(self):
+        return []
+
+    def gradouts(self):
+        return self._gradouts
+
+    def create(self, parent, qm):
+        ...
+
+    def parse(self, qm):
+        ...
+
+    def check(self):
+        ...
+
+    def hessouts(self):
+        return []
+
+
+def minval(itr, value=None):
+    try:
+        return min(itr)
+    except ValueError:
+        return value
+
 
 class AdditionalStructures(Colt):
 
-    def __init__(self, creators):
+    _user_input = """
+    energy_weight = 1 :: int :: >1
+    gradient_weight = 1 :: int :: >1
+    hessian_weight = 1 :: int :: >1
+    """
+
+    def __init__(self, creators, energy_weight, gradient_weight, hessian_weight):
         self.creators = creators
-    
+        self.energy_weight = energy_weight
+        self.gradient_weight = gradient_weight
+        self.hessian_weight = hessian_weight
+
+    def add_hessian(self, weight, hessout):
+        self.creators['hessian'] = HessianOutput(weight, hessout)
+
+    def add_dihedrals(self, weight, scans):
+        self.creators['dihedrals'] = DihedralOutput(weight, scans)
+
     @classmethod
     def from_config(cls, config):
         creators = {}
@@ -133,7 +271,7 @@ class AdditionalStructures(Colt):
             _cls = clss.from_config(getattr(config,name))
             if _cls is not None:
                 creators[name] = _cls
-        return cls(creators)
+        return cls(creators, config.energy_weight, config.gradient_weight, config.hessian_weight)
 
     @classmethod
     def _extend_user_input(cls, questions):
@@ -157,3 +295,55 @@ class AdditionalStructures(Colt):
 
         for name, creator in self.creators.items():
             creator.parse(qm)
+
+    def lowest_energy(self, only_hessian=True):
+        minimum = 10000.0
+        if only_hessian is True:
+            for creator in self.creators.values():
+                min_h = minval((out.energy for out in creator.hessouts()), minimum)
+                if min_h < minimum:
+                    minimum = min_h
+            return minimum
+
+        for creator in self.creators.values():
+            min_e = minval((out.energy for out in creator.enouts()), minimum)
+            if min_e < minimum:
+                minimum = min_e
+            min_g = minval((out.energy for out in creator.gradouts()), minimum)
+            if min_g < minimum:
+                minimum = min_g
+            min_h = minval((out.energy for out in creator.hessouts()), minimum)
+            if min_h < minimum:
+                minimum = min_h
+        return minimum
+
+    def normalize(self):
+        emin = self.lowest_energy(only_hessian=True)
+        self.subtract_energy(emin)
+
+    def subtract_energy(self, energy):
+        for creator in self.creators.values():
+            for out in creator.enouts():
+                out.energy -= energy
+            for out in creator.gradouts():
+                out.energy -= energy
+            for out in creator.hessouts():
+                out.energy -= energy
+
+    def enitr(self):
+        for creator in self.creators.values():
+            weight = creator.weight * self.energy_weight
+            for out in creator.enouts():
+                yield weight, out
+
+    def graditr(self):
+        for creator in self.creators.values():
+            weight = creator.weight * self.gradient_weight
+            for out in creator.gradouts():
+                yield weight, out
+
+    def hessitr(self):
+        for creator in self.creators.values():
+            weight = creator.weight * self.hessian_weight
+            for out in creator.gradouts():
+                yield weight, out
