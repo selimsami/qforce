8b35102403ad63ceb2c648683a294b9bedf06744
Maximilian Menger <menger.maximilian@gmail.com>
2024-09-18 15:19:13 +0200

    updated additionalstructure syntax

--- qforce/additionalstructures.py
+++ qforce/additionalstructures.py
@@ -8,16 +8,88 @@ from ase.io import read
 from calkeeper import CalculationIncompleteError
 
 
-class AdditionalStructureCreator(Colt):
+class CalculationStorage:
+    """Basic class to store calculations and calculation results"""
 
-    name = None
-    __classes = {}
+    def __init__(self, calculations=None, results=None, **metadata):
+        if calculations is None:
+            calculations = []
+        self.calculations = calculations
+        if results is None:
+            results = []
+        self.results = results
+        self._meta = metadata
+
+    def get(self, arg, default=None):
+        return self._meta.get(arg, default)
+
+    def __getitem__(self, arg):
+        return self._meta[arg]
+
+
+class CostumStructureCreator(Colt):
+    """Creator class to generate structures for the fitting procedure
+    Basic idea to call the creator in this way:
+
+    pre: optional
+    main: main step, needs to be implemented
+
+    """
 
     def __init__(self, weight):
         self.weight = weight
+        # will be overwritten!
+        self._folder = None
+
+    @property
+    def folder(self):
+        if self._folder is None:
+            raise ValueError("Please setup main folder")
+        return self._folder
+
+    @folder.setter
+    def folder(self, value):
+        self._folder = value
+
+    def setup_pre(self, qm):
+        pass
+
+    def check_pre(self):
+        pass
+
+    def parse_pre(self, qm):
+        pass
+
+    def setup_main(self, qm):
+        raise NotImplementedError
+
+    def check_main(self):
+        raise NotImplementedError
+
+    def parse_main(self, qm):
+        raise NotImplementedError
+
+    def enouts(self):
+        raise NotImplementedError
+
+    def gradouts(self):
+        raise NotImplementedError
+
+    def hessouts(self):
+        raise NotImplementedError
+
+
+class AdditionalStructureCreator(CostumStructureCreator):
+
+    name = None
+    __classes = {}
+    __predefined = ['dihedrals', 'hessian']
+    _user_input = ""
 
     def __init_subclass__(cls, **kwargs):
         super().__init_subclass__(**kwargs)
+        if cls.name in cls.__predefined:
+            raise ValueError(f"Cannot create class with name {cls.name}")
         cls.__classes[cls.name] = cls
         cls._user_input += ("\n# Weight of the structures in the forcefield fit"
                             "\n weight = 1 :: float \n")
@@ -30,13 +102,22 @@ class AdditionalStructureCreator(Colt):
     def get(cls, name):
         return cls.__classes.get(name)
 
-    def create(self, qm):
+    def setup_main(self, qm):
         raise NotImplementedError
 
-    def check(self):
+    def check_main(self):
         raise NotImplementedError
 
-    def parse(self, qm):
+    def parse_main(self, qm):
+        raise NotImplementedError
+
+    def enouts(self):
+        raise NotImplementedError
+
+    def gradouts(self):
+        raise NotImplementedError
+
+    def hessouts(self):
         raise NotImplementedError
 
 
@@ -51,24 +132,10 @@ class StructuresFromFile(AdditionalStructureCreator):
     """
 
     def __init__(self, weight, en_struct, grad_struct, hess_struct):
-        self.weight = weight
-        self._data = {
-                'en': {
-                       'file': en_struct,
-                       'calculations': [],
-                       'results': [],
-                      },
-                'grad': {
-                         'file': grad_struct,
-                         'calculations': [],
-                         'results': [],
-                        },
-                'hess': {
-                         'file': hess_struct,
-                         'calculations': [],
-                         'results': [],
-                        },
-        }
+        super().__init__(weight)
+        self._energy = CalculationStorage(file=en_struct)
+        self._grad = CalculationStorage(file=grad_struct)
+        self._hess = CalculationStorage(file=hess_struct)
 
     @classmethod
     def from_config(cls, config):
@@ -83,67 +150,62 @@ class StructuresFromFile(AdditionalStructureCreator):
         for i, molecule in enumerate(molecules):
             yield i, (molecule.get_positions(), molecule.get_atomic_numbers())
 
-    def create(self, parent, qm):
-        en = self._data['en']
-        grad = self._data['grad']
-        hess = self._data['hess']
+    def setup_main(self, qm):
+        parent = self.folder
 
-        if en['file'] is not None:
+        if self._energy['file'] is not None:
             folder = parent / 'en_structs'
             os.makedirs(folder, exist_ok=True)
-            en['calculations'] = qm.do_sp_calculations(folder, self._fileiter(en['file']))
-        if grad['file'] is not None:
+            self._energy.calculations = qm.do_sp_calculations(folder,
+                                                              self._fileiter(self._energy['file']))
+        if self._grad['file'] is not None:
             folder = parent / 'grad_structs'
             os.makedirs(folder, exist_ok=True)
-            grad['calculations'] = qm.do_grad_calculations(folder, self._fileiter(grad['file']))
-        if hess['file'] is not None:
+            self._grad.calculations = qm.do_grad_calculations(folder,
+                                                              self._fileiter(self._grad['file']))
+        if self._hess['file'] is not None:
             folder = parent / 'hess_structs'
             os.makedirs(folder, exist_ok=True)
-            hess['calculations'] = qm.do_hessian_calculations(folder, self._fileiter(hess['file']))
+            self._hess.calculations = qm.do_hessian_calculations(folder,
+                                                                 self._fileiter(self._hess['file']))
 
-    def check(self):
-        en = self._data['en']
-        grad = self._data['grad']
-        hess = self._data['hess']
-        for calculation in chain(en['calculations'], grad['calculations'], hess['calculations']):
+    def check_main(self):
+        for calculation in chain(self._energy.calculations, self._grad.calculations,
+                                 self._hess.calculations):
             try:
                 _ = calculation.check()
             except CalculationIncompleteError:
                 return calculation
         return None
 
-    def parse(self, qm):
-        en = self._data['en']
-        grad = self._data['grad']
-        hess = self._data['hess']
-
+    def parse_main(self, qm):
         results = []
-        for calculation in hess['calculations']:
+        for calculation in self._hess.calculations:
             hessian_files = calculation.check()
-            results.append(qm._read_hessian(hessian_files))
+            results.append(qm.read_hessian(hessian_files))
 
         en_results = []
-        for calculation in en['calculations']:
+        for calculation in self._energy.calculations:
             files = calculation.check()
-            en_results.append(qm._read_energy(files))
+            en_results.append(qm.read_energy(files))
 
         grad_results = []
-        for calculation in grad['calculations']:
+        for calculation in self._grad.calculations:
             files = calculation.check()
-            grad_results.append(qm._read_gradient(files))
+            grad_results.append(qm.read_gradient(files))
 
-        en['results'] = en_results
-        grad['results'] = grad_results
-        hess['results'] = results
+        self._energy.results = en_results
+        self._grad.results = grad_results
+        self._hess.results = results
 
     def enouts(self):
-        return self._data['en']['results']
+        return self._energy.results
 
     def gradouts(self):
-        return self._data['grad']['results']
+        return self._grad.results
 
     def hessouts(self):
-        return self._data['hess']['results']
+        return self._hess.results
 
 
 class MetaDynamics(AdditionalStructureCreator):
@@ -152,11 +214,11 @@ class MetaDynamics(AdditionalStructureCreator):
 
     _user_input = """
     compute = none :: str :: [none, en, grad]
-    
+
     # Number of frames used for fitting
     n_fit = 100 :: int
     # Number of frames used for validation
-    n_valid = 0 :: int 
+    n_valid = 0 :: int
     # temperature (K)
     temp = 800 :: float :: >0
     # interval for trajectory printout (fs)
@@ -181,11 +243,11 @@ class MetaDynamics(AdditionalStructureCreator):
         for item in ['temp', 'dump', 'step', 'shake']:
             self.xtbinput[item] = config[item]
 
-        self._data = {
-                'en':  {'calculations': [], 'results': []},
-                'grad':  {'calculations': [], 'results': []},
-                'hess':  {'calculations': [], 'results': []},
-        }
+        self._energy = CalculationStorage()
+        self._grad = CalculationStorage()
+        # not used so far
+        self._hess = CalculationStorage()
+        self._md = CalculationStorage()
 
     @classmethod
     def from_config(cls, config):
@@ -199,82 +261,90 @@ class MetaDynamics(AdditionalStructureCreator):
         for i, molecule in enumerate(molecules):
             yield i, (molecule.get_positions(), molecule.get_atomic_numbers())
 
-    def create(self, parent, qm):
-        folder = parent / '0_md'
+    def setup_pre(self, qm):
+        """setup md calculation"""
+        folder = self.folder / '0_md'
         os.makedirs(folder, exist_ok=True)
 
-        calc = qm.xtb_md(folder, self.xtbinput)
+        # setup
+        self._md.calculations = [qm.xtb_md(folder, self.xtbinput)]
 
-        try:
-            files = calc.check()
-        except CalculationIncompleteError:
-            qm.logger.exit(f"Required xtb trajectory file not found in '{folder}'.\n"
-                           'Creating the necessary input file and exiting...\n'
-                           )
+    def check_pre(self):
+        """check that the md calculation was finished"""
+        for calc in self._md.calculations:
+            try:
+                _ = calc.check()
+            except CalculationIncompleteError:
+                return calc
+        return None
 
+    def parse_pre(self, qm):
+        files = self._md.calculations[0].check()
         traj = files['traj']
-        filename = parent / 'xtbmd.xyz'
+        filename = self.folder / 'xtbmd.xyz'
         copy(traj, filename)
+        self._md.results = [{'file': filename}]
+
+    def setup_main(self, qm):
+        # setupt
+        parent = self.folder
+        # currently only one result there
+        filename = self._md.results[0]['file']
 
         traj = self._fileiter(filename)
 
         if self.compute == 'en':
             folder = parent / '1_en_structs'
             os.makedirs(folder, exist_ok=True)
-            self._data['en']['calculations'] = qm.do_sp_calculations(folder, traj)
+            self._energy.calculations = qm.do_sp_calculations(folder, traj)
         elif self.compute == 'grad':
             folder = parent / '1_grad_structs'
             os.makedirs(folder, exist_ok=True)
-            self._data['grad']['calculations'] = qm.do_grad_calculations(folder, traj)
+            self._grad.calculations = qm.do_grad_calculations(folder, traj)
         else:
             raise ValueError("do not know compute method!")
 
-    def check(self):
-        en = self._data['en']
-        grad = self._data['grad']
-        for calculation in chain(en['calculations'], grad['calculations']):
+    def check_main(self):
+        for calculation in chain(self._energy.calculations, self._grad.calculations):
             try:
                 _ = calculation.check()
             except CalculationIncompleteError:
                 return calculation
         return None
 
-    def parse(self, qm):
-        en = self._data['en']
-        grad = self._data['grad']
-
+    def parse_main(self, qm):
         en_results = []
-        for i, calculation in enumerate(en['calculations']):
+        for i, calculation in enumerate(self._energy.calculations):
             files = calculation.check()
             if i < self.n_fit:
-                en_results.append(qm._read_energy(files))
+                en_results.append(qm.read_energy(files))
 
         grad_results = []
-        for i, calculation in enumerate(grad['calculations']):
+        for i, calculation in enumerate(self._grad.calculations):
             files = calculation.check()
             if i < self.n_fit:
-                grad_results.append(qm._read_gradient(files))
+                grad_results.append(qm.read_gradient(files))
 
-        en['results'] = en_results
-        grad['results'] = grad_results
+        self._energy.results = en_results
+        self._grad.results = grad_results
 
     def enouts(self):
-        return self._data['en']['results']
+        return self._energy.results
 
     def gradouts(self):
-        return self._data['grad']['results']
+        return self._grad.results
 
     def hessouts(self):
-        return self._data['hess']['results']
+        return self._hess.results
 
 
-class HessianOutput:
+class HessianOutput(CostumStructureCreator):
 
     def __init__(self, weight, hessout):
-        self.weight = weight
+        super().__init__(weight)
         if not isinstance(hessout, (tuple, list)):
             hessout = [hessout]
-        self.hessout = hessout
+        self._hessout = hessout
 
     def enouts(self):
         return []
@@ -283,22 +353,22 @@ class HessianOutput:
         return []
 
     def hessouts(self):
-        return self.hessout
+        return self._hessout
 
-    def create(self, parent, qm):
+    def setup_main(self, qm):
         pass
 
-    def parse(self, qm):
+    def check_main(self):
         pass
 
-    def check(self):
+    def parse_main(self, qm):
         pass
 
 
-class DihedralOutput:
+class DihedralOutput(CostumStructureCreator):
 
     def __init__(self, weight, gradouts):
-        self.weight = weight
+        super().__init__(weight)
         self._gradouts = gradouts
 
     def enouts(self):
@@ -307,18 +377,17 @@ class DihedralOutput:
     def gradouts(self):
         return self._gradouts
 
-    def create(self, parent, qm):
-        pass
-
-    def parse(self, qm):
-        pass
-
-    def check(self):
-        pass
-
     def hessouts(self):
         return []
 
+    def setup_main(self, qm):
+        pass
+
+    def check_main(self):
+        pass
+
+    def parse_main(self, qm):
+        pass
 
 def minval(itr, value=None):
     try:
@@ -350,10 +419,23 @@ class AdditionalStructures(Colt):
         self._dihedral_weight = dihedral_weight
 
     def add_hessians(self, hessout):
+        """add hessian"""
         self.creators['hessian'] = HessianOutput(self._hessian_weight, hessout)
 
     def add_dihedrals(self, scans):
+        """add computed dihedrals"""
         self.creators['dihedrals'] = DihedralOutput(self._dihedral_weight, scans)
+
+    def register(self, name, creator):
+        """add a new creator"""
+        if not isinstance(creator, CostumStructureCreator):
+            raise ValueError(f"Can not register '{type(creator)}' "
+                             "only register CostumStructureCreator instances")
+        if name == 'dihedrals':
+            creator.weight = self._dihedral_weight
+        elif name == 'hessian':
+            creator.weight = self._hessian_weight
+        self.creators[name] = creator
 
     @classmethod
     def from_config(cls, config):
@@ -377,10 +459,14 @@ class AdditionalStructures(Colt):
         for i, (name, creator) in enumerate(self.creators.items()):
             folder = parent / f'{i}_{creator.name}'
             os.makedirs(folder, exist_ok=True)
-            creator.create(folder, qm)
+            creator.folder = folder
+
+        # prestep
+        for name, creator in self.creators.items():
+            creator.setup_pre(qm)
 
         for name, creator in self.creators.items():
-            cal = creator.check()
+            cal = creator.check(prestep=False)
             if cal is not None:
                 qm.logger.exit(f"Required output file(s) not found in '{cal.folder}' .\n"
                                'Creating the necessary input file and exiting...\nPlease run the '
@@ -389,9 +475,25 @@ class AdditionalStructures(Colt):
                                f"are:\n{cal.missing_as_string()}\n\n\n")
 
         for name, creator in self.creators.items():
-            creator.parse(qm)
+            creator.parse_pre(qm)
 
-    def get_lowest_energy_and_coords(self):
+        # mainstep
+        for name, creator in self.creators.items():
+            creator.setup_main(qm)
+
+        for name, creator in self.creators.items():
+            cal = creator.check_main()
+            if cal is not None:
+                qm.logger.exit(f"Required output file(s) not found in '{cal.folder}' .\n"
+                               'Creating the necessary input file and exiting...\nPlease run the '
+                               'calculation and put the output files in the same directory.\n'
+                               'Necessary output files and the corresponding extensions '
+                               f"are:\n{cal.missing_as_string()}\n\n\n")
+
+        for name, creator in self.creators.items():
+            creator.parse_main(qm)
+
+    def _get_lowest_energy_and_coords(self):
         minimum = np.inf
         coords = None
 
@@ -406,11 +508,13 @@ class AdditionalStructures(Colt):
         return minimum, coords
 
     def normalize(self):
-        emin, coords = self.get_lowest_energy_and_coords()
+        """set all energies to the minimum one"""
+        emin, coords = self._get_lowest_energy_and_coords()
         self.subtract_energy(emin)
         return emin, coords
 
     def subtract_energy(self, energy):
+        """subtract energy from all qm energies, forces and hessian are not affected"""
         for creator in self.creators.values():
             for out in creator.enouts():
                 out.energy -= energy
